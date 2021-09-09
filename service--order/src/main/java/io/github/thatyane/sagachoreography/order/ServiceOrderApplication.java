75de3d3e390946e6b9c3229722d68d23a6f07a5b
Thatyane Pontes <thatyanepontes@gmail.com>
2021-09-09 10:03:55 -0300

    criacao estrutura inicial

---
+++ service--order/src/main/java/io/github/thatyane/sagachoreography/order/ServiceOrderApplication.java
@@ -0,0 +1,12 @@
+package io.github.thatyane.sagachoreography.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceOrderApplication.class);
+    }
+}
---
+++ service--payment/src/main/java/io/github/thatyane/sagachoreography/payment/ServicePaymentApplication.java
@@ -0,0 +1,12 @@
+package io.github.thatyane.sagachoreography.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicePaymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServicePaymentApplication.class);
+    }
+}
